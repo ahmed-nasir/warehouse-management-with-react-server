b47b6b713d4168dd2f9bbe4e7deba969bd27433c
ahmed-nasir <nasirahmed595@gmail.com>
2022-05-07 18:31:44 +0600

    limit item

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
-const port = process.env.PORT ||5000
+const port = process.env.PORT || 5000
 
 const app = express();
 
@@ -14,18 +14,41 @@ app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.y47ea.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  console.log('Database connected')
-  // perform actions on the collection object
-  client.close();
-});
 
 
-app.get('/', (req,res)=>{
+async function run() {
+    try {
+        await client.connect();
+        const itemsCollection = client.db('warehouseDB').collection('Items')
+
+        app.get('/item', async (req, res) => {
+            const query = {};
+            const cursor = itemsCollection.find(query);
+            const items = await cursor.toArray();
+            res.send(items);
+        })
+        
+        app.get('/limitItem', async (req, res) => {
+            const query = {};
+            const cursor = itemsCollection.find(query).limit(6);
+            const items = await cursor.toArray();
+            res.send(items);
+        })
+        
+    }
+    finally {
+
+    }
+
+}
+run().catch(console.dir)
+
+
+
+app.get('/', (req, res) => {
     res.send('Running servier');
 })
 
-app.listen(port,()=>{
+app.listen(port, () => {
     console.log('lisatningto port', port)
 })
